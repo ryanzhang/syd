1f0c95784040524185aaf6b1d6db3015397f2f53
Ryan ZhangCheng <rzhang@redhat.com>
2022-01-27 16:05:54 +0800

    Initial commit

---
+++ project_name/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: UTF-8 -*-
+from .base import BaseClass, base_function
+
+__all__ = ["BaseClass", "base_function"]
---
+++ project_name/__main__.py
@@ -0,0 +1,68 @@
+# -*- coding: UTF-8 -*-
+import argparse 
+import logging  
+
+from . import BaseClass, base_function  # pragma: no cover
+
+# 初始化日志
+logging.basicConfig(
+    level=logging.INFO, format=" %(asctime)s - %(levelname)s- %(message)s"
+)
+
+def main() -> None:  # pragma: no cover
+    """
+    The main function executes on commands:
+    `python -m project_name` and `$ project_name `.
+
+    This is your program's entry point.
+
+    You can change this function to do whatever you want.
+    Examples:
+        * Run a test suite
+        * Run a server
+        * Do some other stuff
+        * Run a command line application (Click, Typer, ArgParse)
+        * List all available tasks
+        * Run an application (Flask, FastAPI, Django, etc.)
+    """
+    parser = argparse.ArgumentParser(
+        description="project_name.",
+        epilog="Enjoy the project_name functionality!",
+    )
+    # This is required positional argument
+    parser.add_argument(
+        "name",
+        type=str,
+        help="The username",
+        default="author_name",
+    )
+    # This is optional named argument
+    parser.add_argument(
+        "-m",
+        "--message",
+        type=str,
+        help="The Message",
+        default="Hello",
+        required=False,
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Optionally adds verbosity",
+    )
+    args = parser.parse_args()
+    print(f"{args.message} {args.name}!")
+    if args.verbose:
+        print("Verbose mode is on.")
+        logging.info("Runing the main function, Allright!")
+
+    print("Executing main function")
+    base = BaseClass()
+    print(base.base_method())
+    print(base_function())
+    print("End of main function")
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
