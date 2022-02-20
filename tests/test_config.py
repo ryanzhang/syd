70de02d420fcaf04854a037f443ec7e6561e287f
Ryan Zhang <rzhang@redhat.com>
2022-02-20 09:13:40 +0800

    upgrade pg sqlalchemy_db_string due to kupy upgrade

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,11 +5,7 @@ from kupy.logger import logger
 def test_properties_been_load():
     logger.info(type(default_setting))
     assert configs is not None
-    assert configs.get("postgres_host").data == "pg-quant-invest"
-    assert configs["postgres_port"].data == "5432"
-    assert configs.get("postgres_user").data == "user"
-    assert configs.get("postgres_password").data == "password"
-    assert configs.get("postgres_database").data == "market"
+    assert configs.get("sqlalchemy_db_string").data != ""
     assert configs.get("tus_token").data != ""
     assert configs.get("log_level").data != ""
     assert configs.get("something_not_exists") is None
